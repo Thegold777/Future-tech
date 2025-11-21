71c510540374226d4cb6c80163eb86f4f734ae8f
Thegold777 <165582387+Thegold777@users.noreply.github.com>
2025-11-21 18:54:22 +0300

    first commit

---
+++ scripts/Header.js
@@ -0,0 +1,10 @@
+class Header {
+    selectors ={
+        root: '[data-js-header]',
+        overlay: '[data-js-header-overlay]',
+        burgerButton: '[data-js-header-burger-button]',
+    }
+    
+}
+
+export default Header
